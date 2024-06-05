519b15532fea98741680535c74d3179f58a2c93d
jesuisgregoire <grigorestefan1111@yahoo.com>
2024-06-05 11:42:59 +0300

    final commit

--- TAO.m
+++ TAO.m
@@ -31,17 +31,16 @@ Kb = 0.014;
 eb = Kb * w;
 Ra = (ea - eb)/ia;
 %%
-
-%TransferFunction
+% Transfer Function
 num = (n1/n2) * K;
 den = [Ra*(J1 + (n1/n2)^2 * J2), K*Kb, 0];
 
-transfer_function = tf(num,den); %for testing purposes
-[A,B,C,D] = tf2ss(num,den);
-Co = ctrb(A,B);
-Obs = obsv(A,C);
-n = size(A,2);
-m = size(B,2);
+transfer_function = tf(num, den); % For testing purposes
+[A, B, C, D] = tf2ss(num, den);
+Co = ctrb(A, B);
+Obs = obsv(A, C);
+n = size(A, 2);
+m = size(B, 2);
 
 if rank(Co) == n(1) && rank(Obs) == n(1)
     disp('Sistemul este controlabil si observabil => REALIZARE MINIMALA');
@@ -49,18 +48,30 @@ else
     disp('NU ESTE O REALIZARE MINIMALA');
 end
 
-Q = eye(n);
-R = eye(m);
+Q = eye(n) * 0.00001;
+R = eye(m) * 0.00001;
+t = 0:0.01:1000; 
+input_amplitude = 0.2; 
+sys = ss(A, B, C, D);
+input_signal = input_amplitude * ones(size(t));
+[y, t, x] = lsim(sys, input_signal, t);
+figure;
+plot(t, y);
+title('Step Response of the State-Space System');
+xlabel('Time (seconds)');
+ylabel('Output');
+grid on;
+
+
 %%
 X = icare(A, B, Q, R);
 F = -R \ B' * X;
 
 disp('Valorile proprii ale sistemului in bucla inchisa:');
 disp(eig(A + B*F));
+%valorile proprii au partea reala < 0 => sistemul este stabil
 disp('Valorile proprii ale solutiei Riccati:');
 disp(eig(X));
-% valorile proprii ale sist in bucla inchisa au partea reala < 0, asadar
-% sistemul este stabilizat
 % val pr ale solutiei X au partea reala > 0, deci sol este pozitiv definita
 
 %% Metoda Vectorilor Schur
@@ -70,17 +81,43 @@ Hf = [A -B/R*B' ;
 [U, S] = schur(Hf);
 [U, S] = ordschur(U, S, 'lhp'); %se ordoneaza matricile U si S a.i. val pr stabile
                                 %ale lui A sa fie asezate in partea stanga
-                                %(coloanele din stanga)
 X_schur = U(n+1:2*n, 1:n) / U(1:n, 1:n); %solutia
-K_schur = -R \ B' * X_schur;             %reactia
+K_schur = R \ B' * X_schur;             %reactia
 
 disp('Norma diferentei dintre sol obtinuta cu MVS si sol obtinuta cu care():');
 disp(norm(X - X_schur));
 % cele doua solutii sunt foarte apropiate (diferenta e de ordinul e-15)
 
 disp('MVS - nr de conditionare:');
-disp(cond(U(1:n, 1:n)));
-% numarul de cnditionare e apropiat de 1, ceea ce indica o buna
-% conditionare numerica a matricei U(1:n, 1:n), deci operatia de inversare a 
-% ei nu este sensibila la schimbari mici in date, prin urmare rezultatul
-% este precis
+disp(cond(U(1:n, 1:n))); % < 10 => Bine conditionata
+
+disp('Valorile priprii ale sistemului in bucla inchisa < 0 => STABIL')
+disp(eig(A-B*K_schur));%valorile proprii au partea reala < 0 => sistemul este stabil
+initial(ss(A-B*K_schur, zeros(size(B)), [eye(n); -K_schur], zeros(n+size(K_schur,1), size(B,2))), [0.2;0.5]);
+
+%% Metoda Vectorilor Schur in caz de avarie
+gamma = 0.1;
+tau = 0.5;
+Af = (1 - gamma) * A;
+Bf = (1 - tau) * B;
+
+%% Metoda Vectorilor Schur
+Hf = [A -B/R*B' ;
+     -Q   -A'   ];
+ 
+[U, S] = schur(Hf);
+[U, S] = ordschur(U, S, 'lhp'); %se ordoneaza matricile U si S a.i. val pr stabile
+                                %ale lui A sa fie asezate in partea stanga
+X_schur = U(n+1:2*n, 1:n) / U(1:n, 1:n); %solutia
+K_schur = R \ B' * X_schur;             %reactia
+
+disp('Norma diferentei dintre sol obtinuta cu MVS si sol obtinuta cu care():');
+disp(norm(X - X_schur));
+% cele doua solutii sunt foarte apropiate (diferenta e de ordinul e-15)
+
+disp('MVS - nr de conditionare:');
+disp(cond(U(1:n, 1:n))); % < 10 => Bine conditionata
+
+disp('Valorile priprii ale sistemului in bucla inchisa < 0 => STABIL')
+disp(eig(A-B*K_schur));%valorile proprii au partea reala < 0 => sistemul este stabil
+initial(ss(A-B*K_schur, zeros(size(B)), [eye(n); -K_schur], zeros(n+size(K_schur,1), size(B,2))), [0.2;0.5]);
